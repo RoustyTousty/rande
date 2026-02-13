6c4f7378edd4c3d0cdf8eb32d6828f2f835a4a6b
RoustyTousty <emils.misisons@gmail.com>
2026-02-14 00:07:07 +0100

    ss

--- src/app/Letter/page.jsx
+++ src/app/Letter/page.jsx
@@ -5,20 +5,23 @@ export default function Page() {
   const letter = `
 Satikt tevi bija kā klausīties dziesmu pirmo reizi un zinot, ka tā būs mana mīļākā.
 
-Tu esi skaista, bet patiesībā tā ir viss neinteresantākā lieta par tevim. Jā, tu esi skaista, bet to jebkurš ar acīm var redzēt. Tu ieej istabā un cilvēki pamana pat necenšoties. Kā enerģīja mainās tikai tāpēc ka parādījies.
+Jā, tu esi skaista, to var pamanīt ikviens. Tu ienāc telpā un visa atmosfēra mainās. Bet tavs skaistums nav tas, kas tevī mani piesaista visvairāk. Tas ir redzams bez pūlēm un neprasa iedziļināties.
 
 Bet lieta tāda, skaistums ir viss vieglākais ko pamanīt. Tais aizņem nulle centību, lai parādītu ar pirkstu. Tas, ko redzu tevī, aizņem uzmanību. Tas nozīmē pazīt tevi, un tā ir tā daļa, kas padara tevi neaizmirstamu.
 
-Es pamanu to kā tavas acis mainās, kad runā par to, kas tev patiešām interesē. Ne tikai to liesmu acīs, bet arī koncentrēšanos, aizrautību, to kā paceļās tava balss. Tas ir kā vērot liesmu, kas aizdegas, pat ja meiģini to noslēpt. Varbūt tu to nepamani, bet tā aizrautība, ir viena no tavā skaistākajām īpašībām.
-
 Es pamanu tavus smieklus. Ne tikai tos pieklājīgos, kad meiģini būt klusa, bet tos īstos. Tos kuri pārņem tevi. Tos kurus meiģini noslēpt, jo domā, ka būs par skaļu. Bet tie smiekli, tie ir tādi, kas liek cilvēkiem smieties vien dzirdot tos. Tie piepilda istabu kā to nespēj nekas cits.
 
-Un tad ir momenti, kurus neviens cits neredz. Tas kā paliec klusa, kad kautkas sāp. Tas kā meiģini saturēt sevi kopā, jo negribi uzgrūst citiem savas problēmas. Tā itkā tev vienmēr jābūt stiprai, taču joprojām kautkā, pat savos klusajos brīžos, tu parādies cilvēkos ar atvertu sirdi. Tādam skaistumam tu nevari uzlikt makeup, nevar nofakot un nevar atkārtot.
+Es pamanu to kā tavs skatiens mainās, kad runā par to, kas tev patiešām interesē. Ne tikai to liesmu acīs, bet arī koncentrēšanos, aizrautību un to kā paceļās tava balss. Tas ir kā vērot liesmu, kas uzlesmoja pati no sevis, pat ja pati mēģini to apslēpt. Varbūt tu to nepamani, bet tā aizrautība, ir viena no tavā skaistākajām īpašībām.
 
-Bet tas kā tu rūpējies, atstāj markas cilvēkos, kuri ir pietiekami laimīgi tevi pazīt. Tavs skaistums varbūt pagriezīs acis, bet tava dvēsele. Tas kā tu mīli, tas kā tu dod un tas kā tu sapņo. Tas liek cilvēkiem palikt. Tas tevi padara neaizmirstamu. 
+Un tad ir momenti, kurus neviens cits neredz. Tas kā paliec klusa, kad kautkas sāp. Tas kā meiģini saturēt sevi kopā, jo negribi uzgrūst citiem savas domas. Tā itkā tev vienmēr jābūt stiprai, taču joprojām kautkā, pat savos klusajos brīžos, tu parādies cilvēkos ar atvertu sirdi. Tādau skaistumu nevar uzkrāsot, nevar nofakot un nevar atkārtot.
 
-Cilvēki doma, ka tevi pazīst tikai redzot tavu seju. Bet, ja viņi pievērstu uzmanību, kaut nedaudz uzmanību, viņi pamanītu, ka tavs izskats ir tikai vāks, ne viss stāsts. Tu esi skaista, jā, bet tā ir viss neinteresantākā lieta par tevim. Tavs īstais skaistums ir vietās, ko citi nekad nepamana. Un tā ir daļa no tevīm, ko nekad nebeidzu mīlēt. Kā klausīties dziesmu pirmo reizi un zinot, ka tā būs mana mīļākā.
-  `;
+Bet tas kā tu rūpējies, atstāj atmiņas cilvēkos, kuri ir pietiekami laimīgi tevi pazīt. Tavs skaistums varbūt pagriezīs acis, bet tava sirds liks palikt. Tas kā tu mīli, tas kā tu dod un tas kā tu sapņo. Tas liek cilvēkiem palikt. Tas tevi padara neaizmirstamu. 
+
+Cilvēki doma, ka tevi pazīst tikai redzot tavu seju. Bet, ja viņi pievērstu uzmanību, kaut nedaudz uzmanību, viņi pamanītu, ka tavs izskats ir tikai virspuse, ne visums. Tu esi skaista, jā, bet tā ir viss neinteresantākā lieta par tevim. Tavs īstais skaistums ir vietās, ko citi nekad nepamana. Un tā ir daļa no tevīm, ko nekad nebeidzu mīlēt. 
+
+Kā klausīties dziesmu pirmo reizi un zinot, ka tā būs mana mīļākā.
+  
+Emīls. (Ne Valodnieks)`;
 
   return (
     <PageWrapper backTo="/">
